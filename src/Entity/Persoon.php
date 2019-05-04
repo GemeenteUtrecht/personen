ee60a0b2cce5c11185758c4ce0e9dddcd56ebb67
Matthias Oliveiro <matthiasoliveiro@gmail.com>
2019-05-04 23:47:32 +0200

    removed ambtenaren verwijzingen

--- src/Entity/Persoon.php
+++ src/Entity/Persoon.php
@@ -182,7 +182,7 @@ class Persoon implements StringableInterface
 	public $identificatie;
 	
 	/**
-	 * Het RSIN van de organisatie waartoe deze Ambtenaar behoord. Dit moet een geldig RSIN zijn van 9 nummers en voldoen aan https://nl.wikipedia.org/wiki/Burgerservicenummer#11-proef. <br> Het RSIN word bepaald aan de hand van de gauthenticeerde applicatie en kan niet worden overschreven
+	 * Het RSIN van de organisatie waartoe deze Persoon behoord. Dit moet een geldig RSIN zijn van 9 nummers en voldoen aan https://nl.wikipedia.org/wiki/Burgerservicenummer#11-proef. <br> Het RSIN word bepaald aan de hand van de gauthenticeerde applicatie en kan niet worden overschreven
 	 *
 	 * @var integer
 	 * @ORM\Column(
@@ -455,7 +455,7 @@ class Persoon implements StringableInterface
 	public $wijzigingsdatum;
 	
 	/**
-	 * Het contact persoon voor deze ambtenaar
+	 * Het contact persoon voor deze Persoon
 	 *
 	 * @ORM\Column(
 	 *     type     = "string",
