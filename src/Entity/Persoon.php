eb92d8918ed7abef3bb87b09424f69979a274798
Matthias Oliveiro <matthiasoliveiro@gmail.com>
2019-05-04 22:33:07 +0200

    personen werkt nog niet, eigenaar verbeterd

--- src/Entity/Persoon.php
+++ src/Entity/Persoon.php
@@ -479,7 +479,7 @@ class Persoon implements StringableInterface
 	public $contactPersoon;
 	
 	/**
-	 * De eigenaar (applicatie) van dit object, wordt bepaald aan de hand van de geauthenticeerde applicatie die de ambtenaar heeft aangemaakt
+	 * Met eigenaar wordt bijgehouden welke  applicatie verantwoordelijk is voor het object, en daarvoor de rechten beheerd en uitgeeft. In die zin moet de eigenaar dan ook worden gezien in de trant van autorisatie en configuratie in plaats van als onderdeel van het datamodel.
 	 *
 	 * @var App\Entity\Applicatie $eigenaar
 	 *
